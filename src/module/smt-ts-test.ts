cfa577966f9bb8d5dfee8ffa660eb8164c7c125b
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-03-21 18:11:08 -0400

    "Fix" linting errors

--- src/module/smt-ts-test.ts
+++ src/module/smt-ts-test.ts
@@ -1,4 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { SMT } from "./config/config";
+
+declare global {
+  interface CONFIG {
+    SMT: typeof SMT;
+  }
+}
+
 Hooks.once("init", () => {
   console.log("SMT-TS | Initializing SMT-TS");
+
+  // Global configuration
+  CONFIG.ActiveEffect.legacyTransferral = false;
+  CONFIG.SMT = SMT;
 });
